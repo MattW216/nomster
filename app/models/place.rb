2841f3ba6a1e03cf041ee4c6faea0c5ddfc94869
Matt <mattwalker216@gmail.com>
2017-05-15 20:45:48 +0000

    Added validation for address and description, also min char limit for name

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: {minimum: 3, too_short: "Must be at least %{count} characters" }
+  validates :address, presence: true
+  validates :description, presence: true
 
   def self.search(page)
     paginate(page: page, per_page: 5).order('id ASC')
